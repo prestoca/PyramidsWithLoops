15b7bc07b6f6f85d435a62b6c3f9ccfb61f59d4f
cme_i <cme_i@10.28.147.83>
2019-09-25 23:45:57 -0400

    execution

--- PyramidsWithLoops/src/main/Main.java
+++ PyramidsWithLoops/src/main/Main.java
@@ -1,4 +1,5 @@
 /*
+ * Assignment 04
  * Christa Preston
  * prestoca@ucmail.uc.edu
  * IS4010 007 App Dev with Java
@@ -12,14 +13,18 @@
     555555555 
 
 *Citations: Nicholson Lecture Loops
+*Big Java: late objects 2e, Horstmann pg 151
 *Other Information:
+*"A programmer is heading out to the grocery store, so his wife tells him "get a gallon of milk, and if they have eggs, 
+*get a dozen." He returns with 13 gallons of milk."
 */
 package main;
 
 public class Main {
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 5; i++) {	//outer for loop number of rows, this entire loop stops at 5
+		for (int i = 0; i < 5; i++) //outer for loop number of rows, this entire loop stops at 5
+		{	
 			
             for (int j=5-i; j>1; j--)	//counting number of spaces for pyramid shape, total rows - i increment
             {  
@@ -29,9 +34,7 @@ public class Main {
 			{
 				System.out.print(i+1);	//the number in pyramid, i increment +1
 			}
-			
 			System.out.println(" "); 
-			
 		}
 	}
 }
